c74292d0340b1334a592efc4f19d4336b272b2e4
Alifa Faruk <af3266@nyu.edu>
2021-03-18 22:08:38 -0400

    finshed functionality of about us

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom'
+// pages
+import AboutUs from "./pages/aboutus"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path='/aboutus' component={AboutUs}></Route>
+    </Router>
   );
 }
-
 export default App;
