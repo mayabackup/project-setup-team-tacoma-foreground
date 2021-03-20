a5c8999cfa3b4d28e328a7444f1c12ee6652ec0e
Alifa Faruk <af3266@nyu.edu>
2021-03-20 16:07:06 -0400

    fixed code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom'
 // pages
 import AboutUs from "./aboutus"
 
-import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom'
 // pages
 import HomePage from "./home"
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
       <Route path='/aboutus' component={AboutUs}></Route>
-=======
       <Route exact path='/' component={HomePage}></Route>
->>>>>>> master
     </Router>
   );
 }
