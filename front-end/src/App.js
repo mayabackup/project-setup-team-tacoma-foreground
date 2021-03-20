42171891e6e6934ec7b464d25c3a30da2f8d3916
AdamEthan <ae1639@nyu.edu>
2021-03-20 16:42:29 -0400

    Linked Hamburger Menu

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-
+import React from 'react'
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom'
 // pages
 import HomePage from "./home"
+import Navbar from './Navbar.js'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Route exact path='/' component={HomePage}></Route>
     </Router>
   );
