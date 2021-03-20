43780e64626c907def3f427772cbe79451e5b8e5
AdamEthan <ae1639@nyu.edu>
2021-03-20 19:16:54 -0400

    Added MeatTheTeam and fixed logo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,12 +4,15 @@ import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-rout
 // pages
 import HomePage from "./home"
 import Navbar from './Navbar.js'
+import MeetTheTeam from './MeetTheTeam.js'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Route exact path='/' component={HomePage}></Route>
+      <Route exact path='/' component={HomePage}>
+      </Route>
+      <Route path='/MeetTheTeam' component={MeetTheTeam}></Route>
     </Router>
   );
 }
