570633ce1f0642da35fea4a566b9d922f02d7139
Rajan Bharaj <rtb325@nyu.edu>
2021-03-22 16:40:06 -0400

    flex container

--- front-end/src/flight.js
+++ front-end/src/flight.js
@@ -13,7 +13,7 @@ function book(){
 function Flight(props) {
     return (
         <article className="flight">
-            <div className="flight_format">
+            <div className="flight_format" className="flex-container">
                 <h4>Flight #{props.details.flight}</h4>
                 <h4>From: {location}</h4>
                 <h4>To: {destination}</h4>
