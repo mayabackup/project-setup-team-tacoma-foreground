e596c65d850571c2124764fa1a71acfa370493ef
Alifa Faruk <af3266@nyu.edu>
2021-04-03 23:23:28 -0400

    fixing errors

--- back-end/app.js
+++ back-end/app.js
@@ -17,9 +17,64 @@ app.post("/", (req, res) => {
   
 });
 
+<<<<<<< Updated upstream
 app.post("/toplocations", (req, res) => {
   
 });
+=======
+
+app.get("/", async(req, res) => {
+  //const airports=await api.airports()
+ let air= await axios.get('https://raw.githubusercontent.com/mwgg/Airports/master/airports.json')
+ //air=JSON.stringify(air.data)
+ //air= JSON.parse(air.data)
+  res.send({ message:air.data})
+  userData={
+    citizenship:null,
+    location:null,
+    airport:null,
+
+    //advanced; may be null
+    advanced: null,
+    continent: null,
+    reason: null,
+    name:null,
+    email: null,
+     
+  }
+})
+
+app.post('/', (req,res)=>{
+  userData={
+    // do error checkings
+    citizenship:req.body.citizenship,
+    location: req.body.location,
+    airport:req.body.airport.selectedOption.value,
+
+    //advanced; may be null
+    advanced: req.body.advanced,
+    continent: req.body.continent,
+    reason: req.body.reason,
+    name:req.body.name,
+    email: req.body.email,
+     
+  }
+  res.redirect('/confirmation');
+})
+
+
+app.get('/confirmation',(req,res)=>{
+  console.log("sending info to the confirmation page")
+  res.send({message:userData})
+})
+
+//Get request for flight info
+app.get('/flight_info',(req,res)=>{
+  console.log("sending info to the Flight Information page")
+  res.send({message:userData})
+})
+
+>>>>>>> Stashed changes
 
 // export the express app we created to make it available to other modules
 module.exports = app;
--- back-end/covid.js
+++ back-end/covid.js
@@ -43,7 +43,7 @@ function api() {
   axios
     .get("https://covid.ourworldindata.org/data/owid-covid-data.json")
     .then(response => {
-      const v = JSON.stringify(response);
+      const v = CircularJSON.stringify(response);
       const data = JSON.parse(v);
       const filtered = JSON.parse(JSON.stringify(data["data"]));
       for (let x in filtered) {
@@ -67,6 +67,10 @@ function api() {
     })
     .catch(error => {
       console.error(error);
+<<<<<<< Updated upstream
+=======
+      console.log("ERROR api")
+>>>>>>> Stashed changes
     });
 }
 //getter func for MASTER COVID data
@@ -79,3 +83,85 @@ module.exports = {
   api: api,
   getCovid: getCovid
 };
+<<<<<<< Updated upstream
+=======
+
+
+//Code for WebScraping Internal and International Movement Controls:
+
+const csv=require('csv-parser');
+const fs = require('fs');
+const { Console } = require("console");
+
+const resultWeb = {};
+
+// retrieves MAST COVID DATA 
+function api2() {
+  // use date mod
+
+  let today = new Date();
+  let dd = today.getDate() - 1;
+  //NOTE: this will get last month's Date because government policies are not updated frequently.
+  let mm = today.getMonth();
+  let yyyy = today.getFullYear();
+  console.log("entering");
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  lastMonth = yyyy + mm + dd;
+  console.log(lastMonth);
+
+  // axios to retrieve online data
+  axios
+    .get("https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv")
+    .then(response => {
+      const csv1=response.data
+      fs.writeFile('response.csv', csv1, function(err){
+        console.log(err);
+    });
+
+   const filepath='./response.csv'
+   fs.createReadStream(filepath).on('error', ()=>{})
+  .pipe(csv())
+  .on('data', (row) => {
+    if(row["Date"]==lastMonth){
+      
+      resultWeb[row["CountryName"]] = {
+        Workplace: row["C2_Workplace closing"],
+        Internal: row["C7_Restrictions on internal movement"],
+        International: row["C8_International travel controls"]
+      };
+    }
+  })
+  .on('end', () => {
+    console.log("Parsed through CSV File");
+    
+   // console.log(resultWeb)
+
+  });
+      return resultWeb;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+    //you must delete response.csv file after reading it
+    fs.unlinkSync('./response.csv');
+
+}
+//getter func for MASTER COVID data
+function getWebScrape() {
+  return resultWeb;
+}
+// export the express app we created to make it available to other modules
+
+module.exports = {
+  api2: api2,
+  getWebScrape: getWebScrape
+};
+>>>>>>> Stashed changes
