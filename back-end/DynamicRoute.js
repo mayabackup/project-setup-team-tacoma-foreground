2bc5bb5e208558a891197864624648e232f59e4e
rhaberli <rmh444@nyu.edu>
2021-04-15 23:02:05 -0400

    Create DynamicRoute.js
    Attempting to create dynamic routes for the top locations, featured locations, and covid info pages.

---
+++ back-end/DynamicRoute.js
@@ -0,0 +1,42 @@
+
+import App from '/App.js';
+
+function errorhandler(error) {
+	console.error("Dynamic page doesn't esist", error)
+}
+
+function routeloader(route) {
+	return (module) => route(null, module.customroute)
+}
+
+export customroute {
+	component: App,
+    childRoutes: [
+    {
+      path: 'FeaturedLocations',
+      getComponent(location, route) {
+        System.import('pages/FeaturedLocations')
+          .then(routeLoader(route))
+          .catch(errorhandler);
+      }
+    },
+    {
+      path: 'Top Locations',
+      getComponent(location, cb) {
+        System.import('pages/top_locations')
+          .then(routeLoader(route))
+          .catch(errorhandler);
+      }
+    },
+    {
+      path: 'Covid Info',
+      getComponent(location, cb) {
+        System.import('pages/covid_info')
+          .then(routeLoader(route))
+          .catch(errorhandler);
+      }
+    },
+  ]
+};
+		
+	
