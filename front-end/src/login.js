6ded3e7978b5849271aabb085cbf400f3d18deac
Maida Sijaric <maidasijaric@Maidas-MacBook-Pro.local>
2021-04-19 17:21:43 -0400

    w/route

--- front-end/src/login.js
+++ front-end/src/login.js
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from 'react'
 import './login.css'
 
 const login = () => {
+  //states
+  const [login, setLogin] = useState(null);
+  const [password, setPass] = useState(null);
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    localStorage.setItem('login',login)
+    localStorage.setItem('password',password)
+
+    let formData={
+      login,
+      password,
+    }
+    const post= async() => await axios
+    .post('http://localhost:5000/',formData)
+    .then(() => console.log('Sent form data'))
+    .catch(err => {
+      console.error(err);
+    });
+    post()
+  };
 
 return (
   <div className="login">
@@ -12,7 +33,10 @@ return (
     <label>
             <input className="login"
               type="text"
-              placeholder="Enter your login"              
+              placeholder="Enter your login"     
+              value={login}
+              required
+              onChange={e => setLogin(e.target.value)}         
             />
     </label>
     <div className = "space_between"></div>
@@ -20,6 +44,9 @@ return (
             <input className="login"
               type="text"
               placeholder="Enter your password"
+              value={password}
+              required
+              onChange={e => setPass(e.target.value)}
             />
     </label>
     <button button onClick='temp' id="margin3">Log in</button>
